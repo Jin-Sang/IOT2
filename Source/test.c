9bb99fc2425a4ba99f8b78dfc39271e45b92b3f7
Jin-Sang <80252681+Jin-Sang@users.noreply.github.com>
2022-06-04 18:24:41 +0900

    Create test.c

---
+++ Source/test.c
@@ -0,0 +1,18 @@
+
+#include <sys/types.h>
+
+#include <sys/stat.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+int main() {
+
+	char *alphP = "ABC";
+
+	
+	int i = 2;
+
+	printf("%c", alphP[i]);
+}
